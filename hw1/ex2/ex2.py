8660b0a4832c0d68348bd837a24eb36f5f9add96
Michele Veronesi <michele.veronesi@hotmail.com>
2022-11-08 16:24:13 +0100

    submission ready

--- hw1/ex2/ex2.py
+++ hw1/ex2/ex2.py
@@ -1,3 +1,7 @@
+import psutil
+import time
+import argparse
+import uuid
 import redis
 
 
@@ -5,7 +9,7 @@ class RedisDB:
     def __init__(self, host: str,
             port: int,
             user: str,
-            password: str) -> None:
+            password: str):
         self.host = host
         self.port = port
         self.user = user
@@ -20,11 +24,10 @@ class RedisDB:
                 username=self.user,
                 port=self.port,
                 password=self.password
-            )
+                )
             self.connected = self.redis_client.ping()
             return self.connected
-        else:
-            return False
+        return False
 
     def disconnect(self) -> bool:
         if self.redis_client is not None:
@@ -32,15 +35,13 @@ class RedisDB:
             self.redis_client = None
             self.connected = False
             return True
-        else:
-            return False
+        return False
 
     def is_connected(self) -> bool:
         if self.redis_client is not None:
             self.connected = self.redis_client.ping()
             return self.connected
-        else:
-            return False
+        return False
 
     def create_ts(self, key: str,
             compression: bool=True,
@@ -74,7 +75,8 @@ class RedisDB:
                 self.redis_client.ts().add(
                     key=key,
                     timestamp=timestamp,
-                    value=value)
+                    value=value
+                    )
                 return True
             except redis.ResponseError:
                 pass
@@ -91,8 +93,84 @@ class RedisDB:
                     dest_key=dest_key,
                     bucket_size_msec=bucket_size_msec,
                     aggregation_type=aggregation_type   
-                )
+                    )
                 return True
             except redis.ResponseError:
                 pass
         return False
+
+
+def main(args: dict) -> None:
+    MAC_ADDRESS = hex(uuid.getnode())
+    TS_BATTERY = f'{MAC_ADDRESS}:battery'
+    TS_POWER = f'{MAC_ADDRESS}:power'
+    TS_PLUGGED_SEC = f'{MAC_ADDRESS}:plugged_seconds'
+    db = RedisDB(
+        host=args.host, 
+        port=args.port,
+        user=args.user,
+        password=args.password
+        )
+    print(db.connect())
+    db.create_ts(
+        key=TS_BATTERY,
+        retention_msecs=int(32768e5) # appr. 2275 days, 5MB
+        )
+    db.create_ts(
+        TS_POWER,
+        retention_msecs=int(32768e5)
+        )
+    db.create_ts(
+        TS_PLUGGED_SEC,
+        retention_msecs=655360*24*60*60*1000 # appr. 1795 years, 1MB
+        )
+    db.create_rule_ts(
+        source_key=TS_POWER,
+        dest_key=TS_PLUGGED_SEC,
+        bucket_size_msec=24*60*60*1000, # one record per day
+        aggregation_type='sum'
+        )
+    try:
+        print('Starting populating the DB, press CTRL+C to stop')
+        while True:
+            value = psutil.sensors_battery()
+            timestamp = int(time.time()*1000)
+            db.add_value_ts(
+                key=TS_BATTERY,
+                timestamp=timestamp,
+                value = value.percent
+                )
+            db.add_value_ts(
+                key=TS_POWER,
+                timestamp=timestamp,
+                value = int(value.power_plugged)
+                )
+            print(f'Added values\nbattery: {value.percent}\npower:{int(value.power_plugged)}')
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print('\nBye')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--host',
+        type=str,
+        help='The Redis Cloud host',
+        )
+    parser.add_argument(
+        '--port',
+        type=int,
+        help='The Redis Cloud port',
+        )
+    parser.add_argument(
+        '--user',
+        type=str,
+        help='The Redis Cloud user',
+        )
+    parser.add_argument(
+        '--password',
+        type=str,
+        help='The Redis Cloud password',
+        )
+    main(parser.parse_args())
