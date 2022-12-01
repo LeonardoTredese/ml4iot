1c3dd55d6530054f988a36fcfa6d3d1cb06e83ad
mveronesi <michele.veronesi@hotmail.com>
2022-12-01 19:14:22 +0100

    fix absolute path

--- hw2/train.py
+++ hw2/train.py
@@ -255,4 +255,7 @@ if __name__ == '__main__':
         default='results',
         help='Folder for saving csv results.'
     )
-    main(parser.parse_args())
+    args = parser.parse_args()
+    args.models_folder = os.path.join(args.dataset, args.models_folder)
+    args.results_folder = os.path.join(args.dataset, args.results_folder)
+    main(args)
