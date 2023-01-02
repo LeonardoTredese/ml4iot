e417b57057007299efd31e32745c468f9380f992
LeonardoTredese <leonardo.tredese@studenti.unipd.it>
2023-01-02 15:36:53 +0100

    exercise one

---
+++ hw3/publisher.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+import uuid 
+import json
+import time
+import psutil as psu
+
+TOPIC = 's302294'
+client = mqtt.Client()
+client.connect('mqtt.eclipseprojects.io', 1883)
+
+
+def battery_message() -> dict:
+    battery = psu.sensors_battery()
+    return {
+        'mac_address': hex(uuid.getnode()),
+        'timestamp': round(time.time() * 1000),
+        'battery_level': battery.percent,
+        'power_plugged': battery.power_plugged
+    }
+
+while True:
+    msg_dict = battery_message()
+    client.publish(TOPIC, json.dumps(msg_dict))
+    time.sleep(1)
+    
